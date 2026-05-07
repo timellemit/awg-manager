6e6d0c1bdac85f67991271a4b2413631cb656ca5
hoaxisr <avs@rusimp.com>
2026-05-07 15:38:53 +0000

    fix(singbox): hide ghost-terminal import when not installed

--- frontend/src/lib/components/singbox/SingboxGhostTerminal.svelte
+++ frontend/src/lib/components/singbox/SingboxGhostTerminal.svelte
@@ -56,6 +56,7 @@
 		</span>
 	</div>
 
+	{#if singboxInstalled}
 	<div class="term-singbox">
 		<textarea
 			class="term-singbox-input"
@@ -93,6 +94,7 @@
 			{/if}
 		{/if}
 	</div>
+	{/if}
 </div>
 
 <style>
