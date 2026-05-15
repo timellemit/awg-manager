feb63c811d1cfc9e1a8b121fc0912e5436b1af72
hoaxisr <avs@rusimp.com>
2026-05-15 13:29:48 +0000

    chore(singbox-router): delete wizard components and store

--- frontend/src/lib/components/singbox-routing/wizard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as WizardModal } from './WizardModal.svelte';
-export { default as WizardEntry } from './WizardEntry.svelte';
-export { runWizard, WizardError } from './wizardOrchestrator';
